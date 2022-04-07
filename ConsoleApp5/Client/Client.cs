371c269e4e0df4dbcf4ccc2301a4b83324658ef1
Александр Ковалев <a.kovalev@2gis.ru>
2022-04-07 16:57:12 +0300

    added client

---
+++ ConsoleApp5/Client/Client.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+
+namespace ConsoleApp5.Client
+{
+    abstract class Client
+    {
+        private static int _numId = 0;
+        protected readonly int Id;
+        protected string FullName;
+        protected List<BankAccount.BankAccount> BankAccounts = new();
+
+        public int Identifier => Id;
+
+        protected Client(string fullName)
+        {
+            _numId += 1;
+            Id = _numId;
+            FullName = fullName;
+        }
+
+        protected bool AddNewBankAccount(BankAccount.BankAccount bankAccount)
+        {
+            BankAccounts.Add(bankAccount);
+            return true;
+        }
+
+        public double GetAllAmountOfMoneyInBankAccounts()
+        {
+            return BankAccounts.Select(x => x.AmountMoney).Sum();
+        }
+
+        public IEnumerable GetAllBankAccounts()
+        {
+            return BankAccounts.Select(x => x.Identify);
+
+        }
+
+        public bool CloseBankAccount(int accountId)
+        {
+            var bankAccount = BankAccounts.FirstOrDefault(x => x.Identify == accountId);
+
+            if (bankAccount != null)
+            {
+                if (bankAccount.CloseAccount())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ ConsoleApp5/Client/VIPClient.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp5.Client
+{
+    class VipClient : ConsoleApp5.Client.Client
+    {
+        public new bool AddNewBankAccount(BankAccount.BankAccount bankAccount)
+        {
+            if (BankAccounts.Count >= 10) return false;
+
+            base.AddNewBankAccount(bankAccount);
+
+            return true;
+        }
+
+        public VipClient(string fullName) : base(fullName) { }
+    }
+}
